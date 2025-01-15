a72d9b40d0fba186bc0580cb4671c9cfed24df70
Sonali-keshri <rsonalikeshri@gmail.com>
2025-01-15 10:59:58 +0530

    updated bubble sort

--- BubbleSort.java
+++ BubbleSort.java
@@ -6,9 +6,9 @@ public class BubbleSort {
         for (int i = 0; i <= n - 2; i++) {
             for (int j = 0; j < n - i - 1; j++) {
                 if (arr[j] > arr[j + 1]) {
-                    int temp = arr[i];
-                    arr[i] = arr[j];
-                    arr[j] = temp;
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
                 }
             }
         }

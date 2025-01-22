7987cd87113bd119ebe2ecb1a25c276c6ea437dd
Sonali-keshri <rsonalikeshri@gmail.com>
2025-01-22 23:27:02 +0530

    MoveAllZerostoEnd

--- MoveAllZeros.java
+++ MoveAllZeros.java
@@ -1,43 +1,57 @@
-import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.*;
 
 public class MoveAllZeros {
 
-    public static int countzero(int[] arr) {
-        int count = 0;
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] == 0) {
-                count++;
-            }
-        }
-        return count;
-    }
- 
-    public static void moveZeros(int[] arr) {
+    // Brute Force Approach
+    public static int[] moveZeros(int[] arr) {
 
         ArrayList<Integer> nonZeroList = new ArrayList<>();
 
-        int zeroCount = countzero(arr);
-        // int[] newArr = new int[arr.length];
+        int[] result = new int[arr.length];
 
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] != 0) {
                 nonZeroList.add(arr[i]);
             }
         }
-        // for (int i = 1; i < zeroCount; i++) {
-        //     arr[nonZeroList.size() + i] = 0;
-        // }
         for (int i = 0; i < nonZeroList.size(); i++) {
-           System.out.print(nonZeroList.get(i) + ",");
+            result[i] = nonZeroList.get(i);
+
         }
-      
+
+        return result;
+
+    }
+    // Optimal One
+    public static int[] moveZeros2(int[] arr) {
+
+        //finidng the first zero
+        int j = -1;
+        for(int  i=0; i<arr.length; i++){
+            if(arr[i] == 0){
+                j = i;
+                break;
+            }
+        }
+        // if there is no zero
+        if(j == -1){
+            return arr;
+        }
+        for(int i = j + 1; i < arr.length ; i++ ){
+            if(arr[i] != 0){
+                int temp  =  arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+                j++;
+            }
+        }
+        return arr;
     }
 
     public static void main(String[] args) {
         int[] arr = { 1, 0, 2, 3, 0, 4, 0, 1 };
-        moveZeros(arr);
-        // int[] result = moveZeros(arr);
-        // System.out.println(Arrays.toString(result));
+        int[] res = moveZeros2(arr);
+        System.out.println(Arrays.toString(res));
+
     }
 }
